961c05d8b49ac8f79d0e1ca16deb6bf841391a6c
VadymPopov <boycristal10@gmail.com>
2024-03-27 11:56:09 -0400

    fix mobile styles

--- app/(site)/providers.tsx
+++ app/(site)/providers.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute='data-theme' defaultTheme='day' enableSystem>
+    <ThemeProvider attribute='data-theme' defaultTheme='night' enableSystem>
       {children}
     </ThemeProvider>
   );
