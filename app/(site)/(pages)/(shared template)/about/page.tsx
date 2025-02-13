96bcc67bf6a2ccfbfd08e01a64d1702430a4aff1
VadymPopov <boycristal10@gmail.com>
2025-02-13 14:21:46 -0500

    refactor: update about, resume

--- app/(site)/(pages)/(shared template)/about/page.tsx
+++ app/(site)/(pages)/(shared template)/about/page.tsx
@@ -17,13 +17,32 @@ export default async function Page() {
               opportunities and actively learning technologies like Next.js —
               this very website was crafted with it!
             </p>
-            <p className='mb-20'>
+            <p className='mb-5'>
+              I love building user-friendly, responsive interfaces and solving
+              real-world problems with clean, efficient code. I believe in
+              continuous learning, collaborating with great teams, and
+              delivering high-quality digital experiences.
+            </p>
+            <p className='mb-5'>
+              When I was a child, I was obsessed with LEGO, and now coding feels
+              like a grown-up version of that. I really enjoy watching how
+              pieces of code and components come together to create a whole
+              infrastructure. And the best feeling in the world? Seeing people
+              use the websites I&apos;ve built and knowing they&apos;re helping
+              make their lives easier and more enjoyable.
+            </p>
+            <p className='mb-5'>
               During my downtime, I&apos;m deeply passionate about sports.
               Officiating basketball games helps me improve my stress
               resistance, and I&apos;m an active player myself, enjoying both
               basketball and tennis. Yoga is also a regular part of my routine,
-              and I&apos;m constantly honing my Spanish skills with a cheerful
-              &quot;¡Hola amigos!&quot;
+              helping me stay centered and focused.
+            </p>
+            <p className='mb-20'>
+              I&apos;m constantly honing my Spanish skills with a cheerful
+              &quot;¡Hola amigos!&quot; and enjoy exploring Toronto&apos;s
+              vibrant coffee scene when I&apos;m not coding or on the court.
+              Stay tuned!
             </p>
             <div>
               <Button route='/contact'>Contact me</Button>
